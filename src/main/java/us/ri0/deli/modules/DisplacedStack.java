3123f16ebd3be85e2c9741bc4da12df0579b756f
underscore-zi <git@little-canada.org>
2025-01-30 22:52:18 -0600

    Enchance DisplacedStack scanner to include ESP to source block, only triggering on activated spawners, and chat notification

--- src/main/java/us/ri0/deli/modules/DisplacedStack.java
+++ src/main/java/us/ri0/deli/modules/DisplacedStack.java
@@ -3,16 +3,19 @@ package us.ri0.deli.modules;
 import meteordevelopment.meteorclient.events.entity.EntityAddedEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.events.world.ChunkDataEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.IntSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.chunk.Chunk;
@@ -20,6 +23,8 @@ import net.minecraft.world.chunk.WorldChunk;
 import us.ri0.deli.Addon;
 import us.ri0.deli.chunkutils.ChunkUtils;
 import us.ri0.deli.esp.Esp;
+import us.ri0.deli.esp.EspOptions;
+import us.ri0.deli.esp.MockSetting;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,10 +42,28 @@ public class DisplacedStack extends Module {
         .defaultValue(true)
         .build()
     );
+
+    private final Setting<Boolean> requireActivatedSpawner = sgGeneral.add(new BoolSetting.Builder()
+        .name("require-activated-spawner")
+        .description("Only scan dungeons with activated spawners")
+        .defaultValue(false)
+        .visible(() -> doDungeonScan.get())
+        .build()
+    );
+
     private final Setting<Boolean> doMinecartScan = sgGeneral.add(new BoolSetting.Builder()
         .name("scan-minecarts")
         .description("Scan the area around minecarts for displaced blocks")
         .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<Integer> maximumYDistance = sgGeneral.add(new IntSetting.Builder()
+        .name("maximum-y-distance")
+        .description("The maximum distance between the bottom of the stack and the source block. This helps filter out obvious false positives")
+        .defaultValue(30)
+        .min(1)
+        .sliderMax(256)
         .build()
     );
 
@@ -59,6 +82,59 @@ public class DisplacedStack extends Module {
         .defaultValue(5)
         .min(1)
         .sliderMax(32)
+        .build()
+    );
+
+    private final Setting<Boolean> espSource = sgGeneral.add(new BoolSetting.Builder()
+        .name("esp-source")
+        .description("Highlights the source block of the stack")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<SettingColor> lineColor = sgGeneral.add(new ColorSetting.Builder()
+        .name("line-color")
+        .description("Color of lines around block that triggered the scan")
+        .defaultValue(new SettingColor(255, 255, 255))
+        .visible(espSource::get)
+        .build()
+    );
+
+    private final Setting<SettingColor> boxColor = sgGeneral.add(new ColorSetting.Builder()
+        .name("box-color")
+        .description("Color of box around block that triggered the scan")
+        .defaultValue(new SettingColor(255, 0, 255, 30, true))
+        .visible(espSource::get)
+        .build()
+    );
+
+    private final Setting<Boolean> tracer = sgGeneral.add(new BoolSetting.Builder()
+        .name("tracer")
+        .description("Draws a line to block that triggered the scan")
+        .defaultValue(true)
+        .visible(espSource::get)
+        .build()
+    );
+
+    private final Setting<SettingColor> tracerColor = sgGeneral.add(new ColorSetting.Builder()
+        .name("tracer-color")
+        .description("Color of the tracer line")
+        .defaultValue(new SettingColor(255, 255, 255, 255, true))
+        .visible(espSource::get)
+        .build()
+    );
+
+    private final Setting<Boolean> doChatNotif = sgGeneral.add(new BoolSetting.Builder()
+        .name("do-chat-notif")
+        .description("Notify in chat when there is a finding")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<Boolean> includeCoords = sgGeneral.add(new BoolSetting.Builder()
+        .name("include-coords")
+        .description("Include coords in chat notification")
+        .defaultValue(true)
         .build()
     );
 
@@ -84,23 +160,46 @@ public class DisplacedStack extends Module {
         // I might want to move this onto its own executor like I do with chunks, but since this is already called
         // with just one entity at (apart from activation) a time I'm not sure its necessary
         if(event.entity instanceof net.minecraft.entity.vehicle.ChestMinecartEntity cart) {
-            if(cart.getVelocity().getX() > 0.5d || cart.getVelocity().getZ() > 0.5d) return;
-            if(scanCache.containsKey(cart.getBlockPos())) return;
+            MeteorExecutor.execute(() -> {scanMinecart(cart);});
+        }
+    }
 
-            int radius = scanRadius.get();
-            int minDisturbance = disturbanceThreshold.get();
+    public void scanMinecart(net.minecraft.entity.vehicle.ChestMinecartEntity cart) {
+        if(cart.getVelocity().getX() > 0.5d || cart.getVelocity().getZ() > 0.5d) return;
+        if(scanCache.containsKey(cart.getBlockPos())) return;
 
-            var results = scanRegion(cart.getBlockPos(), radius, minDisturbance);
-            espResults(results);
+        int radius = scanRadius.get();
+        int minDisturbance = disturbanceThreshold.get();
+
+        var results = scanRegion(cart.getBlockPos(), radius, minDisturbance);
+        if(results.stacks.isEmpty()) return;
+        if(results.minimumYDistance() > maximumYDistance.get()) return;
+        espResults(results);
+
+        if (doChatNotif.get()) {
+            MutableText msg = Text.literal("Minecart with disturbance");
+            if(includeCoords.get()) {
+                msg = msg.append(" at ").append(ChatUtils.formatCoords(results.pos.toCenterPos()));
+            }
+            ChatUtils.sendMsg("DisplacedStack",  msg);
         }
     }
 
     public void espResults(ScanResult res) {
-        if(res.stacks.isEmpty()) return;
         for (var stack : res.stacks) {
             for (BlockPos p : stack) {
                 esp.BlockWithMapColor(p);
             }
+        }
+
+        if (espSource.get()) {
+            var opts = new EspOptions();
+            opts.tracer = new MockSetting<>(tracer.get());
+            opts.tracerColor = new MockSetting<>(tracerColor.get());
+            opts.lineColor = new MockSetting<>(lineColor.get());
+            opts.sideColor = new MockSetting<>(boxColor.get());
+
+            esp.Block(res.pos, opts);
         }
     }
 
@@ -117,7 +216,27 @@ public class DisplacedStack extends Module {
             if (!isInterestingSpawner(pos)) return;
 
             var results = scanRegion(pos, radius, minDisturbance);
+            if(results.stacks.isEmpty()) return;
+
+            if(requireActivatedSpawner.get()) {
+                var entity = mc.world.getBlockEntity(pos, BlockEntityType.MOB_SPAWNER);
+                if (entity.isEmpty()) return;
+
+                var spawner = (MobSpawnerBlockEntity) entity.get();
+                if(spawner.getLogic().spawnDelay == 20) return;
+            }
+
+            if(results.stacks.isEmpty()) return;
+            if(results.minimumYDistance() > maximumYDistance.get()) return;
             espResults(results);
+
+            if (doChatNotif.get()) {
+                MutableText msg = Text.literal("Dungeon with disturbance");
+                if(includeCoords.get()) {
+                    msg = msg.append(" at ").append(ChatUtils.formatCoords(results.pos.toCenterPos()));
+                }
+                ChatUtils.sendMsg("DisplacedStack",  msg);
+            }
         });
 
     }
@@ -129,7 +248,7 @@ public class DisplacedStack extends Module {
      * @param radius radius around the position to center
      * @param minDisturbance how many displaced blocks are needed before reporting
      */
-    private ScanResult scanRegion(BlockPos pos, int radius, int minDisturbance) {
+    public ScanResult scanRegion(BlockPos pos, int radius, int minDisturbance) {
         ScanResult res = new ScanResult(pos);
         if (scanCache.containsKey(pos)) return scanCache.get(pos);
 
@@ -236,10 +355,10 @@ public class DisplacedStack extends Module {
 
     public void onDeactivate() {
         esp.clear();
+        scanCache.clear();
     }
 
-
-    private class ScanResult {
+    public class ScanResult {
         public final BlockPos pos;
         public final List<List<BlockPos>> stacks;
 
@@ -250,6 +369,20 @@ public class DisplacedStack extends Module {
 
         public void add(List<BlockPos> stack) {
             stacks.add(stack);
+        }
+
+        public int minimumYDistance() {
+            int min = Integer.MAX_VALUE;
+            for (List<BlockPos> stack : stacks) {
+                if(stack.isEmpty()) continue;
+                var firstBLock = stack.getFirst();
+                var dist = Math.abs(pos.getY() - firstBLock.getY() - pos.getY());
+                if (dist < min) {
+                    min = dist;
+                }
+            }
+            return min;
+
         }
     }
 
