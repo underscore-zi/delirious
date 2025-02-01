f9d5557f5aa56142bb99ce20397c3c6e882673be
underscore-zi <git@little-canada.org>
2025-01-31 18:44:08 -0600

    Default Esp to render for 2x player render distance. Bit of a hack until I can rework the esp system so caching and ESP values expire at the same time.

--- src/main/java/us/ri0/deli/esp/EspOptions.java
+++ src/main/java/us/ri0/deli/esp/EspOptions.java
@@ -4,6 +4,8 @@ import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
 public class EspOptions {
     public Setting<Boolean> tracer;
     public Setting<SettingColor> tracerColor;
@@ -19,6 +21,6 @@ public class EspOptions {
         lineColor = new MockSetting<SettingColor>(new SettingColor(255, 0, 0));
         sideColor = new MockSetting<SettingColor>(new SettingColor(255, 0, 0, 50));
         mode = new MockSetting<ShapeMode>(ShapeMode.Both);
-        renderDistance = new MockSetting<Integer>(15);
+        renderDistance = new MockSetting<Integer>(mc.options.getViewDistance().getValue() * 2);
     }
 }
